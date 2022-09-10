68466090de8104b75135d53c13c1a70d186743eb
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-09-09 20:55:49 -0700

    Created Search Engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         Test test test
-        </p>
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <Dictionary />
+        <footer className="App-footer">
+          <small>Coded by Carolyn Partsch</small>
+        </footer>
+      </div>
     </div>
   );
 }
