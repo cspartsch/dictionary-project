7474b9f1649c9a9a4af2c329e7d2ac459e98723b
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-09-17 22:22:42 -0700

    Added Pictures API to Page

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Phonetics.css"
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
   return (
     <div className="Phonetic">
       <a href={props.phonetics.audio} target="#blank">
